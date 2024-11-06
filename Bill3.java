c838e3dcda1d62099901998584c42fad4122dbe1
תמר פלד <Tamar-Peled>
2024-11-06 22:56:52 +0200

    Deleting comments

--- Bill3.java
+++ Bill3.java
@@ -1,13 +1,13 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
         int totalPrice= Integer.parseInt(args[3]);
         double priceForEach;
-	    // Replace this comment with the rest of your code   
+	  
 		
         priceForEach = Math.ceil((double)totalPrice/3);
 
