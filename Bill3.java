5bfc4ebef98fce297acdd66eeff0923fb78e0371
תמר פלד <Tamar-Peled>
2024-11-07 21:31:14 +0200

    Fixes in Bill3 and FVCalc

--- Bill3.java
+++ Bill3.java
@@ -1,17 +1,19 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-
+		//Names 
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-        int totalPrice= Integer.parseInt(args[3]);
-        double priceForEach;
-	  
 		
-        priceForEach = Math.ceil((double)totalPrice/3);
+		//The total price for all the 3
+        int totalPrice= Integer.parseInt(args[3]);
+		
+		// The price for each
+        double priceForEach;
+		priceForEach = Math.ceil((double)totalPrice/3);
 
-       System.out.println("Dear " +name3+ ", "+name2+ " and " +name1+ ": please pay "  +priceForEach+ " shekels each.");
+       System.out.println("Dear " +name3+ ", "+name2+ ", and " +name1+ ": please pay "  +priceForEach+ " shekels each");
 
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,17 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+
+		int currentValue = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[2]);
+		double rate =Double.parseDouble(args[1]);
+		double rate2 = rate/100;
+
+		
+		double futureValue;
+
+		futureValue=currentValue*(Math.pow((1+rate2),n)); 
+
+		System.out.println("After " +n+ " years, a $" +currentValue+ " saved at "+rate+ "% will yield $" +(int)futureValue);
 	}
 }
