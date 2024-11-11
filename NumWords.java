ea7fc2ace74586a28ba74ca54a4b0b84072129aa
תמר פלד <Tamar-Peled>
2024-11-11 17:30:51 +0200

    Spelling error correction

--- NumWords.java
+++ NumWords.java
@@ -1,17 +1,14 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		// Replace this comment with your code
 
 		int inputNumber = Integer.parseInt(args[0]);
 
-		int hundredsDigit = inputNumber/100;
-		int tensDigit = (inputNumber/10)%10;
-		int onesDigit = inputNumber%10;
-		
-		
+		int hundredsDigit = inputNumber / 100;
+		int tensDigit = (inputNumber / 10) % 10;
+		int onesDigit = inputNumber % 10;
 
-
-		System.out.println(hundredsDigit+ " hundreds, " +tensDigit+ " tens, and " +onesDigit+ " Ones.");
+		System.out.println(hundredsDigit + " hundreds, " + tensDigit + " tens, and " + onesDigit + " ones.");
 	}
 }
